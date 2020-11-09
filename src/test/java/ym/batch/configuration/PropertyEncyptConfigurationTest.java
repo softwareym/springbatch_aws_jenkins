f3c01b171ed87a59c7c118ee19e270b29aa03d4b
softwareym <software2126@gmail.com>
2020-11-09 17:30:06 +0900

    localdatetime change

--- src/test/java/ym/batch/configuration/PropertyEncyptConfigurationTest.java
+++ src/test/java/ym/batch/configuration/PropertyEncyptConfigurationTest.java
@@ -23,10 +23,10 @@ public class PropertyEncyptConfigurationTest extends TestCase {
 
         //테스트 코드를 실행해서 아래 xoxo문자열로 batchpwd란 문자열을 암호화한 데이터를 콘솔에서 확인할 수 있다.
         //이렇게 확인한 키를 application.properties에 ENC(암호화된키)로 저장하여 사용할 수 있다.
-        encryptor.setPassword("qfkFclseeogGMHMInM5T9naoiRhtjxGK6feqrIz4WqK4Nw68DkyuSLQlwVghnRLKg0HFCIGughqC7f3WFMHKgQ%3D%3D");                                      //암호화에 사용할 키 -> 중요
+        encryptor.setPassword("batchpwd");                                      //암호화에 사용할 키 -> 중요
         encryptor.setAlgorithm("PBEWithSHA256And128BitAES-CBC-BC");
 
-        String plainText = "qfkFclseeogGMHMInM5T9naoiRhtjxGK6feqrIz4WqK4Nw68DkyuSLQlwVghnRLKg0HFCIGughqC7f3WFMHKgQ%3D%3D";                                      //암호화 할 문자열
+        String plainText = "batchpwd";                                      //암호화 할 문자열
         String encryptedText = encryptor.encrypt(plainText);                //암호화된 키
         String decryptedText = encryptor.decrypt(encryptedText);            //복호화된 키=plainText
 
