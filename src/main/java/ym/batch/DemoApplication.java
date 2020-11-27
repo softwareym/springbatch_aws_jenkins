e978fbc8d2349aed9c30d0c892402f986c54812f
softwareym <software2126@gmail.com>
2020-11-27 19:33:46 +0900

    exit after job

--- src/main/java/ym/batch/DemoApplication.java
+++ src/main/java/ym/batch/DemoApplication.java
@@ -5,19 +5,25 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+//@EnableWebMvc
 @MapperScan(basePackages = "ym.batch.job.*")
 @EnableBatchProcessing  //배치기능 활성화
-@EnableWebMvc
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(DemoApplication.class, args);
-        SpringApplication application = new SpringApplication(DemoApplication.class);
-        application.addListeners(new ApplicationPidFileWriter());
-        application.run(args);
+      /*
+      //boot.pid를 생성 하는 방법(kill -9 `cat boot.pid`로 셧다운 가능)
+          SpringApplication application = new SpringApplication(DemoApplication.class);
+          application.addListeners(new ApplicationPidFileWriter());
+          application.run(args);
+      */
+
+      ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+      System.exit(SpringApplication.exit(context)); //완료시 자동 shutdown
     }
 
 }
